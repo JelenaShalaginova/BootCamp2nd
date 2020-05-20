2158bedae83f9b99756557023f3644628447bf08
Jelena Shalaginova <jelena.shalaginova@gmail.com>
2020-05-20 17:43:41 +0300

    changes to all classes

--- src/main/java/jtm/activity04/Transport.java
+++ src/main/java/jtm/activity04/Transport.java
@@ -15,6 +15,12 @@ public class Transport {
 	 * values of the newly created object
 	 * And make fuel tank full.
 	 */
+	public Transport(String id, float consumption, int tankSize) {
+		this.setId(id);
+		this.consumption = consumption;
+		this.tankSize = tankSize;
+		this.fuelInTank = tankSize;
+	}
 
 
 	/*- TODO #2
@@ -22,6 +28,23 @@ public class Transport {
 	 * and generate public getters for consumption, tankSize, id, and
 	 * fuelInTank fields
 	 */
+	public String getId() {
+		return id;
+	}
+
+	public float getConsumption() {
+		return consumption;
+	}
+
+	public int getTankSize() {
+		return tankSize;
+	}
+
+
+	public float getFuelInTank() {
+		return fuelInTank;
+	}
+
 
 	/*- TODO #3
 	 * Select menu: Source — Generate toString()...
@@ -31,27 +54,57 @@ public class Transport {
 	 * HINT: use String.format(Locale.US, "%.2f", float) to limit shown digits
 	 * to 2 decimal for fractions, and dot as a decimal delimiter.
 	 */
-
+	
+	@Override
+	public String toString() {
+		
+		return String.format(Locale.US, "Id:%s cons:%.1fl/100km, tank:%dl, fuel:%.2fl", getId(), consumption, tankSize, fuelInTank);
+		
+	}
 
 	// Return transport id and type as string e.g. "AAA Transport"
 	// HINT: use this.getClass().getSimpleName(); to get type of transport
 	protected final String getType() {
 		// TODO return required value
-		return "";
+		String type = getId() +" "+ this.getClass().getSimpleName();
+		
+		return type;
 	}
+
+
+	
+	
 
 	// HINT: use getType() to describe transport and road.toString() to describe
 	// road
 	// HINT: String.format(Locale.US, "%.2f", float) to format float number with
 	// fixed mask
 	public String move(Road road) {
+		
+		
+		float minFuel = (this.consumption * road.getDistance()) / 100;
+		
+		if(fuelInTank >= minFuel) {
+		 fuelInTank -= minFuel;
+			return String.format(Locale.US, "%s is moving on %s", getType(),road.toString());
+
+		}else {
+			
+			return String.format(Locale.US, "Cannot move on %s. Necessary fuel:%.2fl, fuel in tank:%.2fl",road.toString(), minFuel, fuelInTank);
+		}
 		// TODO If transport has enough fuel, decrease actual amount of fuel by
 		// necessary amount and return String in form:
-		// "AAA Type is moving on From–To, 180km"
+		// "AAA Type is moving on Fromâ€“To, 180km"
 		// TODO If there is no enough fuel in tank, return string in form:
-		// "Cannot move on From–To, 180km. Necessary
+		// "Cannot move on Fromâ€“To, 180km. Necessary
 		// fuel:0.00l, fuel in tank:0.00l"
-		return "";
+		
+		
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 }

baa599702322bb6c6c0e1a49a12a3302186475b0
Jelena Shalaginova <jelena.shalaginova@gmail.com>
2020-05-26 10:34:02 +0300

    act07 vers1

---
+++ src/main/java/jtm/activity07/Dog.java
@@ -0,0 +1,19 @@
+package jtm.activity07;
+
+public class Dog extends Mammal{
+	
+	private String name;
+	
+	
+	public void setName(String name) {
+		this.name = name;
+		
+	}
+	
+	public String getName() {
+		return name;
+		
+	}
+	
+
+}//end class
---
+++ src/main/java/jtm/activity07/Mammal.java
@@ -0,0 +1,17 @@
+package jtm.activity07;
+
+public class Mammal extends Animal {
+
+	private boolean isDomestic;
+	
+	public void setIsDomestic(boolean isDomestic) {
+		this.isDomestic = isDomestic;	
+		
+	}
+	
+	public boolean getIsDomestic() {
+		return isDomestic;
+		
+	}
+	
+}//end class

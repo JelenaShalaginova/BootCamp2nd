301226c84be15a865f44d324470ee6fadcf5235e
Jelena Shalaginova <jelena.shalaginova@gmail.com>
2020-05-30 11:27:52 +0300

    activity done, 1 test not passed :(

---
+++ src/main/java/jtm/activity08/SimpleCalcException.java
@@ -0,0 +1,16 @@
+package jtm.activity08;
+
+public class SimpleCalcException extends Exception {
+
+	public SimpleCalcException(String string, Throwable cause) {
+		super(string, cause);
+		
+	}
+
+	public SimpleCalcException(String string) {
+		super(string);
+	
+	}
+
+	
+}//end class
